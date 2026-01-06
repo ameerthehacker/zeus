46849b4f18bb014f76d77299f39ab6658e1e4088
Ameer Jhan <ameerjhanprof@gmail.com>
2026-01-06 15:28:28 +0530

    docs: finalize pages

--- docs/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Z1fvh7J from "../../assets/zeus-hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/zeus-hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1fvh7J]]);
-		
